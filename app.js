dc1fb3ce7642255790fc620cec0c3aff1d3b7680
obotha <khyper234@gmail.com>
2019-03-11 16:14:23 +0300

    Test with electron table

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-import 'babel-polyfill'; // required to make promises work
-import mysql from 'mysql';
+import queryTable from './utils/read_modify';
 
-const func = async () => {
-    const connection = await mysql.createConnection(
-        {
-            host:'localhost', 
-            user: 'jmuhumuza', 
-            password: 'joshua', 
-            database: 'wdrDb',
-            multipleStatements: true,
-        });
-
-
-    const QUERY = 'SELECT * FROM Electron WHERE id=1';
-    connection.query(QUERY, (queryError, result, fields) => {
-        if (queryError) {
-          throw queryError;
-        } else {
-            console.log(result);
-        }
-    });
-
-    connection.end((connectionError) => {
-        console.log(connectionError);
-    });
-}
-
-func();
-console.log('outsyd function');
+queryTable('Electron');
