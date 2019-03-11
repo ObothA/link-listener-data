6cf3dc9f23692f18af0ba43539f9885d0a693001
obotha <khyper234@gmail.com>
2019-03-11 17:19:36 +0300

    Give up on imports

--- app.js
+++ app.js
@@ -1,3 +1,74 @@
-import { queryTable } from '../utils/read_modify';
+import 'babel-polyfill'; // required to make promises work
+import mysql from 'mysql';
+
+
+const queryTable = async (table) => {
+    const connection = await mysql.createConnection(
+        {
+            host:'localhost', 
+            user: 'jmuhumuza', 
+            password: 'joshua', 
+            database: 'wdrDb',
+            multipleStatements: true,
+        });
+
+
+    const QUERY = `SELECT id,NAME,stationname FROM ${table} WHERE stationID=111`;
+    connection.query(QUERY, (queryError, result, fields) => {
+        if (queryError) {
+            throw queryError;
+        } else {
+            assignStationId(result,connection,table);
+        }
+    });
+
+    connection.end((disConnectionError) => {
+        if(disConnectionError){
+            console.log(disConnectionError);
+        }
+    });
+}
+
+
+const assignStationId = (result, connection, table) => {
+    result.map((dbField) => {
+        const { id,NAME, stationname } = dbField;
+
+        let stationName;
+
+        if(!NAME && stationname){
+            stationName = stationname; 
+        }
+        
+        if (NAME && NAME.includes('-')) {
+            stationName = NAME.split('-')[0];
+            var stationNumber = NAME.split('-')[1];
+            if (!isNaN(stationNumber)) {
+                stationName = `${stationName}-${stationNumber}`;
+            }
+        } else if (NAME && NAME.includes('_')) {
+            stationName = NAME.split('_')[0];
+            var stationNumber2 = NAME.split('_')[1];
+            if (!isNaN(stationNumber2)) {
+                stationName = `${stationName}_${stationNumber2}`;
+            }
+        }
+
+        const STATION_NAMES = {
+            'myg' : 54,
+            'makg3' : 53,
+            'kml' : 52,
+            'jja' : 50,
+            'byd-2' : 49,
+            'byd-1' : 48
+        };
+
+        console.log(STATION_NAMES[stationName]);
+
+        const updateQuery = `UPDATE ${table} SET stationID = '${StationID}' WHERE id = ${id}`;
+
+
+    });
+}
 
 queryTable('Electron');
