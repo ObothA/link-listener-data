21b7939788930f5473485b9a6d0c2de2d253e270
obotha <khyper234@gmail.com>
2019-03-13 18:47:56 +0300

    Automate the entire process

--- app.js
+++ app.js
@@ -2,30 +2,38 @@ const mysql = require('mysql');
 const queryTable = require('./utils/queryTable');
 
 
-function App(){
-const connection = mysql.createConnection({
-        host:'localhost', 
-        user: 'jmuhumuza', 
-        password: 'joshua', 
+function App() {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'jmuhumuza',
+        password: 'joshua',
         database: 'wdrDb',
         multipleStatements: true,
     });
 
-// queryTable('Electron');
-// queryTable('GeneralTable');
 
-var status = true;
-status = queryTable('GroundNode', connection);
-
-setInterval(function(){
-    if(!status){
-        queryTable('GroundNode', connection);
-    } else {
-        console.log('######## table is linked  ########');
+    var table_counter = 1;
+    var tables = {
+        1 : 'Electron',
+        2 : 'GeneralTable',
+        3 : 'GroundNode',
+        4 : 'SinkNode',
+        5 : 'TenMeterNode',
+        6 : 'TwoMeterNode'
     }
-}, 5000);
+
+    var status = true;
+    status = queryTable(tables[table_counter], connection);
+
+    setInterval(function () {
+        if (!status) {
+            queryTable(tables[table_counter], connection);
+        } else {
+            console.log(`########  ${tables[table_counter]} is linked  ########`);
+            table_counter++;
+            status = false;
+        }
+    }, 5000);
 }
-
-
 
 App();
